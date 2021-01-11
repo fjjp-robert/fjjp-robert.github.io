92623d48c656ee58a3618908ca58aea3261ee1e1
fred.robert <f.robert17@gmail.com>
2021-01-11 12:00:17 +0100

    commit pokedux test

---
+++ precache-manifest.dc7d55aa5f40f2ed2e54733886bc638a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d074159a126b0a2e037cd7392a262895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef6affef1d3da3d4917e",
+    "url": "/static/css/main.ce216b91.chunk.css"
+  },
+  {
+    "revision": "ce91bcaef6226e0e9026",
+    "url": "/static/js/2.e148c2f0.chunk.js"
+  },
+  {
+    "revision": "ef6affef1d3da3d4917e",
+    "url": "/static/js/main.204961f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
